0d22a33582e7e02cf3deeb4104006e80514502ba
Ronald Pereira Evangelista <Ronald.Evangelista@Aluno.ifsp.edu.br>
2024-10-13 16:45:30 -0300

    email update v3

--- CsCrudApi/Models/UserRelated/Request/EmailVerification.cs
+++ CsCrudApi/Models/UserRelated/Request/EmailVerification.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CsCrudApi.Models.UserRelated.Request
 {
-    [Table("EmailVerification")]
+    [Table("emailverification")]
     public class EmailVerification
     {
         [Required]
