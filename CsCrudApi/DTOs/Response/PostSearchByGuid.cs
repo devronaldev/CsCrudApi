1d737ab5cf358c8d806e4c1babe4afe304d8b1c0
Ronald Pereira Evangelista <Ronald.Pereira.Evangelista@gmail.com>
2025-05-28 18:00:53 -0300

    Feat: Novos DTOs para especificos Uses Cases.

---
+++ CsCrudApi/DTOs/Response/PostSearchByGuid.cs
@@ -0,0 +1,24 @@
+﻿using CsCrudApi.Models.PostRelated;
+using CsCrudApi.Models.UserRelated;
+
+namespace CsCrudApi.DTOs;
+
+public record PostSearchByGuidDTO
+{
+    public int UserId { get; set; }
+    public string Name { get; set; }
+    public EGrauEscolaridade GrauEscolaridade { get; set; }
+    public string ProfilePicture { get; set; }
+    public string Instituicao { get; set; }
+    public PostResponseDTO Post { get; set; }
+
+    public PostSearchByGuidDTO(CampusResponseDTO c, User u, PostResponseDTO p)
+    {
+        UserId = u.UserId;
+        Name = u.NmSocial;
+        GrauEscolaridade = u.GrauEscolaridade;
+        ProfilePicture = u.ProfilePictureUrl;
+        Instituicao = $"{c.SgCampus} - {c.Name}";
+        Post = p;
+    }
+};
