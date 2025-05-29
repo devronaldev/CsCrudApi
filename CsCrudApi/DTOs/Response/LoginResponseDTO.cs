c10b9ebf4b1d55214b947be620fef8a25add70ec
Ronald Pereira Evangelista <Ronald.Pereira.Evangelista@gmail.com>
2025-05-29 18:11:52 -0300

    Feat: Entidade LoginResponseDTO.cs criada para atuar como um novo retorno para o login e também para o endpoint de refresh (Token Refresh).

---
+++ CsCrudApi/DTOs/Response/LoginResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CsCrudApi.DTOs;
+
+public record LoginResponseDTO
+{
+    public string AccessToken { get; set; }
+    public DateTime AccessExpiresAt { get; init; } = DateTime.Now;
+    public DateTime RefreshExpiresAt { get; init; } = DateTime.Now;
+    public int UserId { get; set; }
+    public string TokenType { get; set; } = "Bearer";
+}
